05758a5f700e634bf1975602d0d326fc2f16b5c3
llgalvao24 <llgalvao24@gmail.com>
2019-10-20 20:20:37 -0300

    User model

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) =>
-  res.json({
-    message: 'Hello, first project!',
-  })
-);
+routes.get('/', async (req, res) => {
+  const adm2 = await User.create({
+    name: 'adm2',
+    email: 'adm2@adm.com',
+    password: '1234567',
+  });
+
+  res.json(adm2);
+});
 
 module.exports = routes;

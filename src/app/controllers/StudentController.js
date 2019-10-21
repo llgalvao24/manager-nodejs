5522ef0b100fc9b0b87a7c9c7b753d64afb097bb
llgalvao24 <llgalvao24@gmail.com>
2019-10-21 14:52:23 -0300

    StudentController & create student

---
+++ src/app/controllers/StudentController.js
@@ -0,0 +1,37 @@
+import * as Yup from 'yup';
+import Student from '../models/Students';
+
+class StudentController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      age: Yup.number()
+        .required()
+        .positive()
+        .integer(),
+      weight: Yup.number().positive(),
+      height: Yup.number().positive(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const studentExists = await Student.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (studentExists) {
+      return res.status(400).json({ error: 'User already exists!' });
+    }
+
+    const student = await Student.create(req.body);
+
+    return res.json(student);
+  }
+}
+
+export default new StudentController();
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import StudentController from './app/controllers/StudentController';
 import SessionController from './app/controllers/SessionController';
-import Student from './app/models/Students';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -15,16 +15,18 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
-routes.get('/', async (req, res) => {
-  const student = await Student.create({
-    name: 'Luiz',
-    email: 'llgalvao@gmail.com',
-    age: 29,
-    weight: 84.5,
-    height: 1.67,
-  });
+routes.post('/students', StudentController.store);
 
-  res.json(student);
-});
+// routes.get('/', async (req, res) => {
+//   const student = await Student.create({
+//     name: 'Luiz',
+//     email: 'llgalvao@gmail.com',
+//     age: 29,
+//     weight: 84.5,
+//     height: 1.67,
+//   });
+
+//   res.json(student);
+// });
 
 module.exports = routes;
